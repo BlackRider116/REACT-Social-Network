c5b48d895f9c2362f954c860badaef061ab99b83
student <valiev.ilnur@bk.ru>
2020-04-13 23:33:48 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/REACT-first-progect/precache-manifest.fec2ed093b782adcc0d5744f2bc2bdd1.js"
+  "/REACT-first-progect/precache-manifest.9c0c73b45ca1301d677fe70881562a4e.js"
 );
 
 self.addEventListener('message', (event) => {
